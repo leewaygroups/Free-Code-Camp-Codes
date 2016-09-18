bb5851bef2a694eac9f25b2c5016674b23930672
leewaygroups <leewaygroups@gmail.com>
2016-09-18 16:56:23 +0700

    Trivial cleanup

--- intermediateFrontEndProjects/twitchTv/src/components/twitchMain-react.js
+++ intermediateFrontEndProjects/twitchTv/src/components/twitchMain-react.js
@@ -39,22 +39,3 @@ var Twitch = React.createClass({
 });
 
 module.exports = Twitch;
-
-//https://api.twitch.tv/kraken/streams?stream_type=all
-
-/*<div className="panel panel-primary">
-    <div className="panel-heading">
-          <TwitchHeader />
-    </div>
-    <div className="panel-body">
-        <p>Streams</p>
-    </div>
-    <div className="container-fluid">
-        <TwitchList twiches={this.state.twitches} />
-    </div>
-    <div className="panel-footer clearfix">
-        <div className="pull-right">
-          <a href="#" className="btn btn-default">Go Twitch</a>
-      </div>
-    </div>
-</div>*/
